c3f9bf4428791acb5e82ef76bf024394ae0c96cd
Michael Rommel <michamaxe@gmail.com>
2021-01-07 22:00:16 +0100

    add startup script

--- smarttimeline/urls.py
+++ smarttimeline/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('timeline/', include('timeline.urls')),
@@ -23,3 +26,6 @@ urlpatterns = [
     path('roadmap/', include('roadmap.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
